4bd728566b84eb1f967c0b2ac7b3fbb4f695c406
Jackon <jiekunyang@gmail.com>
2015-08-23 20:53:09 +0800

    grab reviews

--- dianping.py
+++ dianping.py
@@ -2,10 +2,10 @@
 import os
 from os.path import join, exists
 import re
-from crawler.request import request
+from crawler.request import request, request_pages
 from settings import cache_root
 
-page_types = ['user_prof', 'shop_prof', 'review']
+page_types = ['user_prof', 'shop_prof', 'reviews']
 for t in page_types:
     if not exists(join(cache_root, t)):
         os.makedirs(join(cache_root, t))
@@ -13,6 +13,7 @@ for t in page_types:
 uid_ptn = re.compile(r'href="/member/(\d+)(?:\?[^"]+)?"')
 sid_ptn = re.compile(r'href="/shop/(\d+)(?:\?[^"]+)?"')
 rev_ptn = re.compile(r'<li[^>]+id="rev_(\d+)"')
+find_rev = lambda c, key: set(rev_ptn.findall(c))
 
 
 def grab_user_prof(key):
@@ -29,11 +30,20 @@ def grab_shop_prof(key):
     return set(sid_ptn.findall(c)) - {key}
 
 
+def grab_reviews(key, max_page=100):
+    url = 'http://www.dianping.com/shop/{key}/review_more?pageno={page}'
+
+    filename_ptn = join(cache_root, 'reviews', '{}_{}.html')  # key, page
+    return request_pages(key, range(1, max_page), url, find_rev,
+                         filename_ptn=filename_ptn)
+
+
 if __name__ == '__main__':
-    # from test_tools import request
+    # from test_tools import request, request_pages
 
     uid = '3601131'
     print grab_user_prof(uid)
 
     sid = '5195730'  # 45 reviews on 2015.8.3
     print grab_shop_prof(sid)
+    print grab_reviews(sid)
--- test_tools.py
+++ test_tools.py
@@ -9,3 +9,16 @@ def request(url, filename):
     with open('testdata.html', 'r') as f:
         text = ''.join(f.readlines())
     return text
+
+
+def request_pages(key, page_range, url_ptn, find_items, resend=3,
+                  min_num=0, max_failed=5, filename_ptn=None):
+    print '\n---------------- requesting pages ------------'
+    for i in page_range[:3]:  # first 3 pages
+        print 'url: {}'.format(url_ptn.format(key=key, page=i))
+        print 'filename: {}'.format(filename_ptn.format(key, i))
+    print ''
+
+    with open('testdata.html', 'r') as f:
+        text = ''.join(f.readlines())
+    return find_items(text, key)
