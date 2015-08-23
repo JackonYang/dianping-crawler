3e09432ae97aa28ed833952de0007e7a8568da7f
Jackon <jiekunyang@gmail.com>
2015-08-23 21:28:26 +0800

    mkdir and get dir with dict

--- dianping.py
+++ dianping.py
@@ -6,9 +6,10 @@ from crawler.request import request, request_pages
 from settings import cache_root
 
 page_types = ['user_prof', 'shop_prof', 'reviews']
-for t in page_types:
-    if not exists(join(cache_root, t)):
-        os.makedirs(join(cache_root, t))
+page_path = {name: join(cache_root, name) for name in page_types}
+for path in page_path.values():
+    if not exists(path):
+        os.makedirs(path)
 
 uid_ptn = re.compile(r'href="/member/(\d+)(?:\?[^"]+)?"')
 sid_ptn = re.compile(r'href="/shop/(\d+)(?:\?[^"]+)?"')
@@ -18,14 +19,14 @@ find_rev = lambda c, key: set(rev_ptn.findall(c))
 
 def grab_user_prof(key):
     url = 'http://www.dianping.com/member/{}'.format(key)
-    fn = join(cache_root, 'user_prof', '{}_user.html'.format(key))
+    fn = join(page_path['user_prof'], '{}_user.html'.format(key))
     c = request(url.format(key), filename=fn)
     return set(uid_ptn.findall(c)) - {key}
 
 
 def grab_shop_prof(key):
     url = 'http://www.dianping.com/shop/{}'.format(key)
-    fn = join(cache_root, 'shop_prof', '{}_shop.html'.format(key))
+    fn = join(page_path['shop_prof'], '{}_shop.html'.format(key))
     c = request(url.format(key), filename=fn)
     return set(sid_ptn.findall(c)) - {key}
 
@@ -33,13 +34,13 @@ def grab_shop_prof(key):
 def grab_reviews(key, max_page=100):
     url = 'http://www.dianping.com/shop/{key}/review_more?pageno={page}'
 
-    filename_ptn = join(cache_root, 'reviews', '{}_{}.html')  # key, page
+    filename_ptn = join(page_path['reviews'], '{}_{}.html')  # key, page
     return request_pages(key, range(1, max_page), url, find_rev,
                          filename_ptn=filename_ptn)
 
 
 if __name__ == '__main__':
-    # from test_tools import request, request_pages
+    from test_tools import request, request_pages
 
     uid = '3601131'
     print grab_user_prof(uid)
